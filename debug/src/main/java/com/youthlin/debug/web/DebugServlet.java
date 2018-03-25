205ef2d50f35f93f808bdda8cdaf936e72b4ca04
youthlin.chen <youthlin.chen@qunar.com>
2018-03-25 22:07:02 +0800

    debug

--- debug/src/main/java/com/youthlin/debug/web/DebugServlet.java
+++ debug/src/main/java/com/youthlin/debug/web/DebugServlet.java
@@ -78,7 +78,7 @@ public class DebugServlet extends HttpServlet {
         out.println("    <meta http-equiv='X-UA-Compatible' content='IE=edge'>");
         out.println("    <meta name='viewport' content='width=device-width, initial-scale=1'>");
         out.println("    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->");
-        out.println("    <title>列表页</title>");
+        out.println("    <title>Debug</title>");
         out.println("    <!-- Bootstrap -->");
         out.println("    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->");
         out.println("    <link rel='stylesheet' href='https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'>");
